95004f0363ffceee855be56ac2167ad4651ee6dd
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-10-12 21:03:58 -0700

    increment

--- todo-actix/src/main.rs
+++ todo-actix/src/main.rs
@@ -1,8 +1,7 @@
 mod models;
 
-use actix_web::{HttpServer, App, web, Responder};
+use actix_web::{web, App, HttpServer, Responder};
 use std::io;
-
 
 async fn status() -> impl Responder {
     "{\"status\": \"UP\"}"
@@ -10,11 +9,8 @@ async fn status() -> impl Responder {
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(status))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/", web::get().to(status)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
