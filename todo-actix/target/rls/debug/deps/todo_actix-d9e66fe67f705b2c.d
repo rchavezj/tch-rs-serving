6441fa479772c41600ce8ab302422249b02bbe06
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-10-14 07:35:00 -0700

    Setting up configuration

---
+++ todo-actix/target/rls/debug/deps/todo_actix-d9e66fe67f705b2c.d
@@ -0,0 +1,6 @@
+/Users/rchavezj/Github/rust_graphql/todo-actix/target/rls/debug/deps/todo_actix-d9e66fe67f705b2c.rmeta: src/main.rs src/models.rs
+
+/Users/rchavezj/Github/rust_graphql/todo-actix/target/rls/debug/deps/todo_actix-d9e66fe67f705b2c.d: src/main.rs src/models.rs
+
+src/main.rs:
+src/models.rs:
---
+++ todo-actix/target/rls/debug/deps/yaml_rust-e47d7cc72d563c41.d
@@ -0,0 +1,9 @@
+/Users/rchavezj/Github/rust_graphql/todo-actix/target/rls/debug/deps/yaml_rust-e47d7cc72d563c41.rmeta: /Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/lib.rs /Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/emitter.rs /Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/parser.rs /Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/scanner.rs /Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/yaml.rs
+
+/Users/rchavezj/Github/rust_graphql/todo-actix/target/rls/debug/deps/yaml_rust-e47d7cc72d563c41.d: /Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/lib.rs /Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/emitter.rs /Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/parser.rs /Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/scanner.rs /Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/yaml.rs
+
+/Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/lib.rs:
+/Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/emitter.rs:
+/Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/parser.rs:
+/Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/scanner.rs:
+/Users/rchavezj/.cargo/registry/src/github.com-1ecc6299db9ec823/yaml-rust-0.4.4/src/yaml.rs:
