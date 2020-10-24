c089936ce4465d0d41c7494f669900f895e2c43a
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-10-23 23:56:44 -0700

    First successful sql table build and hosted on a docker container

--- todo-actix/database.sql
+++ todo-actix/database.sql
@@ -1,18 +1,18 @@
 drop table if exists todo_item;
 drop table if exists todo_list;
 
-create table todo_list {
+create table todo_list (
     id serial primary key,
     title varchar(150)
-}
+);
 
-create table todo_item {
+create table todo_item (
     id serial primary key,
     title varchar(150) not null,
     checked boolean not null default false,
     list_id integer not null,
     foreign key (list_id) references todo_list(id)  
-};
+);
 
 insert into todo_list (title) values ('List 1'), ('List 2');
 
