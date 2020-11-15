99803e0e4f36d366cfc52192ed93b52ab881dfc2
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-11-15 11:06:19 -0800

    Organizing Projects

---
+++ 00_server-from-scratch/src/main.rs
@@ -0,0 +1,33 @@
+
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self { Self { addr } }
+
+    // fn run (&self) {
+
+    // }
+}
+
+
+// &self --> a string slice is an immutable reference to a part of a string. 
+// 
+fn main() {
+    let string = String::from("127.0.0.1:8080");
+    let string_slice = &string[10..];
+    let string_borrow: &str = &string;
+    let string_literal = "1234";
+
+    dbg!(&string);
+    dbg!(&string_slice);
+    dbg!(&string_borrow);
+    dbg!(&string_literal);
+
+    let server = Server::new("127.0.0.1:8080".to_string());
+    // server.run();    
+}
+
+
