2a2003f210d7ad7133f15b7d8033fb3eb543a4b5
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-12-06 22:05:00 -0800

    Setting up http method struct

--- 00_server-from-scratch/src/main.rs
+++ 00_server-from-scratch/src/main.rs
@@ -7,28 +7,39 @@ struct Server {
 impl Server {
     fn new(addr: String) -> Self { Self { addr } }
 
-    fn run (&mut self) {
-        
-    }
+    fn run (self) { println!("Listening on {}", self.addr); }
 }
 
+
+struct Request {
+    path: String,
+    query_string: String,
+    method: String,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
 
 // &self --> a string slice is an immutable reference to a part of a string. 
 // 
 fn main() {
-    let foodieString = String::from("🍔🍟🍻🍻");
-    let string = String::from("127.0.0.1:8080");
-    let string_slice = &string[10..];
-    let string_borrow: &str = &string;
-    let string_literal = "1234";
 
-    dbg!(&string);
-    dbg!(&string_slice);
-    dbg!(&string_borrow);
-    dbg!(&string_literal);
+    let get = Method::GET;
+    let put = Method::PUT;
+    let post = Method::POST;
+    let delete = Method::DELETE;
 
     let server = Server::new("127.0.0.1:8080".to_string());
-    // server.run();    
+    server.run();    
 }
 
 
