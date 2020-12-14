6e609a4ccc567562468a3a9e2f03e4d0cc71d5d3
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-12-13 18:39:17 -0800

    Listening to server

--- 00_server-from-scratch/src/server.rs
+++ 00_server-from-scratch/src/server.rs
@@ -9,6 +9,23 @@ impl Server {
 
     pub fn run (self) { 
         println!("Listening on {}", self.addr); 
-        let listener = TcpListener::bind(&self.addr); 
+        let listener = TcpListener::bind(&self.addr).unwrap(); 
+
+        loop {
+            match listener.accept() {
+                Ok() => {
+                    let a = 5;
+                    println!("OK");
+                },
+                Err() => println!("Err")
+            }
+            let res = listener.accept();
+
+            if res.is_err(){
+                continue;
+            }
+
+            let (stream, addr) = res.unwrap();
+        }
     }
 }
