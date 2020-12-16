28e302aeb688bb74ed67d1b78f397143a12d06e1
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-12-15 21:56:31 -0800

    First successful get http request using tcpListener

--- 00_server-from-scratch/src/server.rs
+++ 00_server-from-scratch/src/server.rs
@@ -1,3 +1,4 @@
+use std::io::Read;
 use std::net::TcpListener;
 
 pub struct Server {
@@ -13,10 +14,18 @@ impl Server {
 
         loop {
             match listener.accept() {
-                Ok((stream, addr)) => {
-                    let a = 5;
-                    println!("OK");
-                },
+                Ok((mut stream, _ )) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer)  {
+                        Ok(_) => {
+                            println!(
+                                "Recieved a request: {}", 
+                                String::from_utf8_lossy(&buffer)
+                            );
+                        }
+                        Err(e) => println!("Failed to establish a connection: {}", e),
+                    }
+                }
                 Err(e) => println!("Failed to establish a connection: {}", e),
             }
         }
