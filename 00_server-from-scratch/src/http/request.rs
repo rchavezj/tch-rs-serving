056ec17e45518fbfe0be2248f4525973252d9760
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-12-15 22:40:56 -0800

    About to implement a conversion TryFrom to support byte to array

--- 00_server-from-scratch/src/http/request.rs
+++ 00_server-from-scratch/src/http/request.rs
@@ -1,7 +1,18 @@
 use super::method::Method;
+use std::convert::TryFrom;
         
 pub struct Request {
     path: String,
     method: Method,
     query_string: Option<String>,
+}
+
+impl Request {
+    fn from_byte_array(buf -> &[u8]) -> Result <Self, String> {
+        
+    }
+}
+
+impl TryFrom for Request {
+    
 }
