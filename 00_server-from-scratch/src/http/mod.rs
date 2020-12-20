c00c21f53479525dd0a95154a13812164f4d8f04
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-12-19 19:00:14 -0800

    ParseError implementation for http request

--- 00_server-from-scratch/src/http/mod.rs
+++ 00_server-from-scratch/src/http/mod.rs
@@ -1,5 +1,5 @@
-pub use request::Request;
 pub use method::Method;
+pub use request::{Request, ParseError};
 
 pub mod method;
 pub mod request;
--- 00_server-from-scratch/src/http/request.rs
+++ 00_server-from-scratch/src/http/request.rs
@@ -1,34 +1,74 @@
-use super::method::Method;
+use std::error::Error;
 use std::convert::TryFrom;
-        
+use super::method::Method;
+use std::fmt::{ 
+    Debug,
+    Display,
+    Formatter, 
+    Result as FmtResult
+};
+
+
 pub struct Request {
     path: String,
     method: Method,
     query_string: Option<String>,
 }
 
+
 impl Request {
     fn from_byte_array(buf -> &[u8]) -> Result <Self, String> { }
 }
 
-impl TryFrom<&[u8]> for Request {
-    type Error = String;
 
+impl TryFrom<&[u8]> for Request {
+    
+    type Error = ParseError;
+
+    // Get /search?name=abc&sort=1 HTTP/1.1
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error>{
         let string = String::from("asd");
         string.encrypt(); 
         buf.encrypt();
         unimplemented!()
     }
-    
 }
 
-// trait Encrypt {
-//     fn encrypt(&self) -> Self;
-// }
 
-// impl Encrypt for &[u8] {
-//     fn encrypt(&self) -> Self {
-//         unimplemented!()
-//     }
-// }
+pub enum ParseError {
+    InvalidRequest,
+    InvalidEncoding,
+    InvalidProtocol,
+    InvalidMethod
+}
+
+
+impl ParseError {
+    fn message(&self) -> &str {
+        match self {
+            Self::InvalidRequest => "Invalid InvalidRequest",
+            Self::InvalidEncoding => "Invalid InvalidEncoding",
+            Self::InvalidProtocol => "Invalid InvalidProtocol",
+            Self::InvalidMethod => "Invalid InvalidMethod"
+        }
+    }
+}
+
+
+impl Display for ParseError{
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message());
+    }
+}
+
+
+impl Debug for ParseError{
+    fn fmt(&self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{}", self.message());
+    }
+}
+
+
+impl Error for ParseError {
+
+}
