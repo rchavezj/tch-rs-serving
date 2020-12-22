dab9294f868f9d3d3785a23ed9d1d22587d2e01d
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2020-12-22 14:07:18 -0800

    PyTorch Rust Bindings Project setup

---
+++ 03_pytorch_tch-rs/target/debug/deps/pytorch_tch_rs-5ef0730bffea3860.d
@@ -0,0 +1,5 @@
+/home/rchavezj/Github/Rust_GraphQL/03_pytorch_tch-rs/target/debug/deps/pytorch_tch_rs-5ef0730bffea3860.rmeta: src/main.rs
+
+/home/rchavezj/Github/Rust_GraphQL/03_pytorch_tch-rs/target/debug/deps/pytorch_tch_rs-5ef0730bffea3860.d: src/main.rs
+
+src/main.rs:
