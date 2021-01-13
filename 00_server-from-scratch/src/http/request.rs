0f6a90b726508c07cf8777b077eae90caf4555be
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2021-01-12 18:48:06 -0800

    Added multipe routing http request inside 'website_handler.rs

--- 00_server-from-scratch/src/http/request.rs
+++ 00_server-from-scratch/src/http/request.rs
@@ -19,6 +19,18 @@ pub struct Request<'buf> {
     query_string: Option<QueryString<'buf>>,
 }
 
+impl<'buf> Request<'buf> {
+    pub fn method(&self) -> &Method{
+        &self.method
+    }
+    pub fn path(&self) -> &str {
+        &self.path
+    }
+    pub fn query_string(&self) -> Option<&QueryString> {
+        self.query_string.as_ref()
+    }
+}
+
 
 impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
--- 00_server-from-scratch/src/website_hander.rs
+++ 00_server-from-scratch/src/website_hander.rs
@@ -1,10 +1,17 @@
-use super::http::{Request, Response, StatusCode};
+use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
 
 pub struct WebsiteHandler; 
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response{
-        Response::new(StatusCode::Ok, Some("<h1>TEST</h1>".to_string()))
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(StatusCode::Ok, Some("<h1>HELLO WORLD :D </h1>".to_string())),
+                "/yolo" => Response::new(StatusCode::Ok, Some("<h1>YOLO :D </h1>".to_string())),
+                _ => Response::new(StatusCode::NotFound, None)
+            }
+            _ => Response::new(StatusCode::NotFound, None)
+        }
     }
 }
