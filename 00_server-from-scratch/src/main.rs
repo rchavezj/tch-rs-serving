1749e638b0e35aa6ba5e68e771019df0bbf178bf
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2021-01-13 19:17:27 -0800

    Finished server from scratch repo

--- 00_server-from-scratch/src/main.rs
+++ 00_server-from-scratch/src/main.rs
@@ -1,8 +1,7 @@
 #![allow(dead_code)]
 use server::Server;
 use website_hander::WebsiteHandler;
-// use http::Method;
-// use http::Request;
+use std::env;
 
 mod http;
 mod server;
@@ -10,7 +9,13 @@ mod website_hander;
 
 // &self --> a string slice is an immutable reference to a part of a string. 
 fn main() {
+    let default_path = format!(
+        "{}/public", 
+        env!("CARGO_MANIFEST_DIR")
+    );
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public_path: {}", public_path);
     let server = Server::new("127.0.0.1:8080".to_string());
-    server.run(WebsiteHandler);    
+    server.run(WebsiteHandler::new(public_path));    
 }
 
--- 00_server-from-scratch/src/website_hander.rs
+++ 00_server-from-scratch/src/website_hander.rs
@@ -1,15 +1,42 @@
 use super::http::{Request, Response, StatusCode, Method};
 use super::server::Handler;
+use std::fs;
 
-pub struct WebsiteHandler; 
+pub struct WebsiteHandler{
+    public_path: String
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+    fn read_file(&self, file_path: &str) -> Option<String>{
+        let path = format!("{}/{}", self.public_path, file_path);
+        
+        match fs::canonicalize(path){
+            Ok(path) => {
+                if path.starts_with(&self.public_path){
+                    fs::read_to_string(path).ok()
+                }else{
+                    println!("Directory Traversal Attack Attempted: {}", file_path);
+                    None
+                }
+            },
+            Err(_) => None,
+        }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &Request) -> Response{
         match request.method() {
             Method::GET => match request.path() {
-                "/" => Response::new(StatusCode::Ok, Some("<h1>HELLO WORLD :D </h1>".to_string())),
-                "/yolo" => Response::new(StatusCode::Ok, Some("<h1>YOLO :D </h1>".to_string())),
-                _ => Response::new(StatusCode::NotFound, None)
+                "/" => Response::new(StatusCode::Ok, self.read_file("index.html") ),
+                "/hello" => Response::new(StatusCode::Ok, self.read_file("hello.html") ),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                }
             }
             _ => Response::new(StatusCode::NotFound, None)
         }
