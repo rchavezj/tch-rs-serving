7d10bb01a75d6aeb42dcb8f5a1f680ed2fa2e5d2
Roberto Chavez Jr <rchavezj.ai@gmail.com>
2021-02-15 21:35:19 -0800

    Successful cargo run when using my personal laptop (Asus Zephyrus G14 2020: Nvidia 2060o + AMD 4000 series). Need to disable cuda on non-nvidia machines

--- 03_pytorch_tch-rs/src/main.rs
+++ 03_pytorch_tch-rs/src/main.rs
@@ -1,9 +1,9 @@
-extern crate tch;
 use tch::Tensor;
+extern crate tch;
 
 fn main() {
     let t = Tensor::of_slice(&[3, 1, 4, 1, 5]);
-    // let _t = Tensor;
-    // let t = t * 2;
+    t.print();
+    let t = t * 2;
     t.print();
 }
